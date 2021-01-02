d0431c39f2dbd39e3e9e023a83e290ff9598a960
Ashleigh Udoh <mail@audoh.co.uk>
2021-01-02 04:31:40 +0000

    default config path is now always relative to the project

--- packman/config.py
+++ packman/config.py
@@ -2,7 +2,14 @@ import os
 
 _DEFAULT_CFG_PATH = "cfg"
 
-DEFAULT_CONFIG_PATH = os.environ.get("PACKMAN_CONFIG_FILE", _DEFAULT_CFG_PATH)
+_dir = os.path.dirname(__file__)
+_dir_to_root = ".."
+_root = os.path.normpath(os.path.join(_dir, _dir_to_root))
+
+
+DEFAULT_CONFIG_PATH = os.environ.get(
+    "PACKMAN_CONFIG_FILE", os.path.normpath(
+        os.path.join(_root, _DEFAULT_CFG_PATH)))
 DEFAULT_MANIFEST_PATH = os.environ.get("PACKMAN_MANIFEST_FILE", "packman.json")
 DEFAULT_GIT_URL = os.environ.get("PACKMAN_GIT_URL",
                                  "https://github.com/audoh/packman.git")

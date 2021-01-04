aa60a3683ca4d2f40ecc888dd10000f5e4a3f745
Ashleigh Udoh <mail@audoh.co.uk>
2021-01-04 03:57:05 +0000

    added log box to gui

--- packman_gui.py
+++ packman_gui.py
@@ -2,7 +2,7 @@ import sys
 import tkinter as tk
 from enum import Enum
 from re import L
-from typing import Iterable, List, Optional, Tuple
+from typing import Iterable, List, NamedTuple, Optional, Tuple, Union
 
 from packman import Packman
 from packman.models.configuration import Package
@@ -24,8 +24,49 @@ class SortKey(Enum):
             return package
 
 
+class LogView(tk.Frame):
+    def __init__(self, master: Optional[tk.Misc] = None, background: Optional[str] = "white", height: Optional[Union[int, str]] = None, width: Optional[Union[int, str]] = None):
+        super().__init__(master, height=str(height)
+                         if height is not None else None, width=str(width) if width is not None else None)
+        self.background = background
+
+        self.el_labels: List[tk.Label] = []
+        self.grid_propagate(0)
+
+        self.el_canvas = tk.Canvas(
+            self, background=background, highlightthickness=1)
+        self.el_canvas.grid(row=0, column=0, sticky=tk.NSEW)
+
+        self.el_scrollbar = tk.Scrollbar(self)
+        self.el_scrollbar.grid(row=0, column=1, sticky=tk.NS)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self.el_canvas.configure(yscrollcommand=self.el_scrollbar.set)
+        self.el_scrollbar.configure(command=self.el_canvas.yview)
+
+        self.el_frame = tk.Frame(self.el_canvas)
+        self.el_canvas.create_window(
+            (0, 0), window=self.el_frame, anchor=tk.NW)
+
+    def line_write(self, text: str, color: Optional[str] = None) -> None:
+        label = tk.Text(self.el_frame, background=self.background, foreground=color,
+                        height=1, relief=tk.FLAT, maxundo=0)
+        label.insert(tk.INSERT, text)
+        label.configure(state=tk.DISABLED)
+        label.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, padx=0, pady=0)
+        self.el_labels.append(label)
+        self.el_canvas.configure(scrollregion=self.el_canvas.bbox("all"))
+
+    def line_clear(self) -> None:
+        for label in self.el_labels:
+            label.pack_forget()
+        self.el_labels.clear()
+
+
 class Menu(tk.Menu):
-    def __init__(self, master=None):
+    def __init__(self, master: Optional[tk.Misc] = None):
         super().__init__(master)
         self.create_submenus()
 
@@ -37,7 +78,7 @@ class Menu(tk.Menu):
 
 class Application(tk.Frame):
 
-    def __init__(self, master=None):
+    def __init__(self, master: Optional[tk.Misc] = None):
         super().__init__(master)
         self.create_widgets()
 
@@ -56,7 +97,7 @@ class Application(tk.Frame):
                                    command=self.update_packman, justify=tk.RIGHT)
         self.el_update.grid(row=0, column=2, sticky=tk.EW)
 
-        self.el_packages = tk.Listbox(self, selectmode="extended")
+        self.el_packages = tk.Listbox(self, selectmode=tk.EXTENDED)
         self.el_packages.grid(row=1, column=0, columnspan=3, sticky=tk.NSEW)
         self.el_packages.bind("<<ListboxSelect>>", self.update_buttons_state)
 
@@ -79,10 +120,8 @@ class Application(tk.Frame):
 
         button_panel.grid(row=2, column=2)
 
-        self.el_success = tk.Label(
-            self, text="succ", relief=tk.GROOVE, fg="green")
-        self.el_error = tk.Label(self, text="fail", relief=tk.GROOVE, fg="red")
-        self.el_info = tk.Label(self, text="info", relief=tk.GROOVE)
+        self.el_output = LogView(self, height=100)
+        self.el_output.grid(row=10, column=0, columnspan=3, sticky=tk.EW)
 
     def update_buttons_state(self, trigger_event: Optional[tk.Event] = None) -> None:
         manifest = self.packman.manifest()
@@ -109,21 +148,16 @@ class Application(tk.Frame):
             self.el_validate.configure(state=tk.DISABLED)
 
     def show_success(self, message: str) -> None:
-        self.el_success.configure(text=message)
-        self.el_success.grid(row=3, column=0, columnspan=3, sticky=tk.EW)
+        self.el_output.line_write(message, color="green")
 
     def show_error(self, message: str) -> None:
-        self.el_error.configure(text=message)
-        self.el_error.grid(row=4, column=0, columnspan=3, sticky=tk.EW)
+        self.el_output.line_write(message, color="red")
 
     def show_info(self, message: str) -> None:
-        self.el_info.configure(text=message)
-        self.el_info.grid(row=5, column=0, columnspan=3, sticky=tk.EW)
+        self.el_output.line_write(message)
 
     def clear_message(self) -> None:
-        self.el_error.grid_forget()
-        self.el_success.grid_forget()
-        self.el_info.grid_forget()
+        self.el_output.line_clear()
 
     def set_packages(self, packages: Iterable[Tuple[str, Package]], sorted=False) -> None:
         self.el_packages.delete(0, tk.END)
@@ -154,46 +188,42 @@ class Application(tk.Frame):
             self.refresh_all_packages()
 
     def install_selected(self) -> None:
-        self.clear_message()
         succeeded: List[str] = []
         failed: List[str] = []
         for name, _ in self.curselection():
             success = self.packman.install(name=name)
             if success:
                 succeeded.append(name)
-                self.show_success(f"installed: {', '.join(succeeded)}")
+                self.show_success(f"installed {name}")
             else:
                 failed.append(name)
-                self.show_error(f"not installed: {', '.join(failed)}")
+                self.show_error(f"did not install {name}")
         self.update_buttons_state()
 
     def uninstall_selected(self) -> None:
-        self.clear_message()
         succeeded: List[str] = []
         failed: List[str] = []
         for name, _ in self.curselection():
             success = self.packman.uninstall(name=name)
             if success:
                 succeeded.append(name)
-                self.show_success(f"uninstalled: {', '.join(succeeded)}")
+                self.show_success(f"uninstalled {name}")
             else:
                 failed.append(name)
-                self.show_error(f"not uninstalled: {', '.join(failed)}")
+                self.show_error(f"did not uninstall {name}")
         self.update_buttons_state()
 
     def validate_selected(self) -> None:
-        self.clear_message()
         invalid_files: List[str] = []
         for name, _ in self.curselection():
             for file in self.packman.validate(name=name):
                 invalid_files.append(file)
                 self.show_error(
-                    f"invalid files: {', '.join(invalid_files)}")
+                    f"invalid file (checksum mismatch): {file}")
         if not invalid_files:
             self.show_success("no invalid files")
 
     def update_packman(self) -> None:
-        self.clear_message()
         if self.packman.update():
             self.refresh_packages()
         else:

ebf5d95a185e8214bcd5bf49ed197735358fe653
Ashleigh Udoh <mail@audoh.co.uk>
2021-01-07 23:28:56 +0000

    copy folder progress support

--- packman/steps/copy_folder.py
+++ packman/steps/copy_folder.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path, PurePath
-from typing import Callable, List, Optional
+from typing import Callable, Dict, List, Optional, Set
 
 from packman.models.install_step import BaseInstallStep, install_step
 from packman.utils.operation import Operation
+from packman.utils.progress import StepProgress
 
 
 @install_step("copy_folder")
@@ -12,7 +13,21 @@ class CopyFolderInstallStep(BaseInstallStep):
     to: str
     exclude: Optional[List[str]] = None
 
-    def copy_folder(self, operation: Operation, src: str, dest: str) -> None:
+    def execute(self, operation: Operation, package_path: str, root_dir: str, on_progress: Callable[[float], None] = lambda p: None) -> None:
+        src = ""
+        dest = ""
+        for root, subdirs, files in os.walk(package_path):
+            for subdir in subdirs:
+                if subdir == self.name:
+                    src = os.path.join(root, subdir)
+                    dest = os.path.join(root_dir, self.to)
+                    break
+            if src:
+                break
+        if not src:
+            raise Exception("folder not found: {self.name}")
+
+        files_to_copy: Dict[str, str] = {}
         for root, subdirs, files in os.walk(src):
             root_relpath = os.path.relpath(root, src)
             dest_root = os.path.join(dest, root_relpath)
@@ -25,14 +40,12 @@ class CopyFolderInstallStep(BaseInstallStep):
                     if any(pure_path.match(pattern) for pattern in self.exclude):
                         continue
                 file_dest = os.path.join(dest_root, file)
-                operation.copy_file(file_src, file_dest)
+                files_to_copy[file_src] = file_dest
 
-    def execute(self, operation: Operation, package_path: str, root_dir: str, on_progress: Callable[[float], None] = lambda: None) -> None:
-        for root, subdirs, files in os.walk(package_path):
-            for subdir in subdirs:
-                if subdir == self.name:
-                    src = os.path.join(root, subdir)
-                    dest = os.path.join(root_dir, self.to)
-                    self.copy_folder(operation, src, dest)
-                    return
-        raise Exception(f"folder not found: {self.name}")
+        step_count = len(files_to_copy)
+        on_step_progress = StepProgress(
+            step_mult=1 / step_count, on_progress=on_progress)
+
+        for file_src, file_dest in files_to_copy.items():
+            operation.copy_file(file_src, file_dest)
+            on_step_progress.advance()

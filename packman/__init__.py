97f3a0d7de6763e4a9abb94d294279df2ac17235
Ashleigh Udoh <mail@audoh.co.uk>
2021-01-13 23:15:14 +0000

    export install_step and package_source decorators

--- packman/__init__.py
+++ packman/__init__.py
@@ -1,2 +1,3 @@
 from packman.manager import (Packman, default_packman, install, packages,
                              uninstall, update, validate, versions)
+from packman.models import install_step, package_source

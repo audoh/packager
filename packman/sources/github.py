c383aa8e1aa2bc51b9ee2b96b32cc4c20f35ba54
Ashleigh Udoh <mail@audoh.co.uk>
2021-01-19 21:46:19 +0000

    updated github source to use new HTTPAPI class

--- packman/sources/github.py
+++ packman/sources/github.py
@@ -1,17 +1,18 @@
-
 import base64
 import json
 import os
 from typing import Any, Dict, Iterable, List, Optional
 from urllib import parse as urlparse
 
-import requests
 from loguru import logger
-from packman.models.package_source import (BasePackageSource, PackageVersion,
-                                           package_source)
+from packman.api.http import HTTPAPI
+from packman.models.package_source import (
+    BasePackageSource,
+    PackageVersion,
+    package_source,
+)
 from packman.utils.operation import Operation
-from packman.utils.progress import (ProgressCallback, StepProgress,
-                                    progress_noop)
+from packman.utils.progress import ProgressCallback, StepProgress, progress_noop
 
 _API_URL = "https://api.github.com"
 
@@ -23,40 +24,33 @@ if "GITHUB_TOKEN" in os.environ:
     _HEADERS["authorization"] = f"Basic {token_base64}"
 
 
-class RepositoryAPI:
-    headers = _HEADERS
-    url = _API_URL
-    _cache: Dict[str, Any] = {}
-
+class RepositoryAPI(HTTPAPI):
     def __init__(self, repository: str) -> None:
+        super().__init__(url=_API_URL)
         self.repository = repository
+        self.headers.update(_HEADERS)
 
     def uri(self, endpoint: str) -> str:
         return urlparse.urljoin(self.url, f"/repos/{self.repository}/{endpoint}")
 
-    def get(self, endpoint: str, use_cache: bool = False, **kwargs: Any) -> requests.Response:
-        cache_key = f"get:{endpoint}?{urlparse.urlencode(kwargs)}"
-        if use_cache and cache_key in self._cache:
-            return self._cache[cache_key]
-
-        res = requests.get(url=self.uri(endpoint),
-                           headers=self.headers, params=kwargs)
-        res.raise_for_status()
-        res_json = res.json()
-        self._cache[cache_key] = res_json
-        return res_json
-
-    def list_releases(self, per_page: Optional[int] = None, page: Optional[int] = None) -> List[Dict[str, Any]]:
+    def list_releases(
+        self, per_page: Optional[int] = None, page: Optional[int] = None
+    ) -> List[Dict[str, Any]]:
         return self.get("releases", per_page=per_page, page=page)
 
-    def list_release_assets(self, release_id: int, per_page: Optional[int] = None, page: Optional[int] = None) -> List[Dict[str, Any]]:
+    def list_release_assets(
+        self,
+        release_id: int,
+        per_page: Optional[int] = None,
+        page: Optional[int] = None,
+    ) -> List[Dict[str, Any]]:
         return self.get(f"releases/{release_id}/assets", per_page=per_page, page=page)
 
     def get_release_by_tag_name(self, tag: str) -> Dict[str, Any]:
         return self.get(f"releases/tags/{tag}", use_cache=True)
 
     def get_latest_release(self) -> Dict[str, Any]:
-        return self.get(f"releases/latest")
+        return self.get("releases/latest")
 
 
 def _to_version_info(release: Dict[str, Any]) -> PackageVersion:
@@ -65,13 +59,14 @@ def _to_version_info(release: Dict[str, Any]) -> PackageVersion:
         version=release["tag_name"],
         description=release["body"],
         options=list(
-            (asset["name"] for asset in release["assets"] if _is_usable_archive(asset)))
+            (asset["name"] for asset in release["assets"] if _is_usable_archive(asset))
+        ),
     )
 
 
 def _is_usable_archive(asset: Dict[str, Any]) -> bool:
-    supported_content_types = ("application/zip")
-    supported_extensions = ("zip")
+    supported_content_types = "application/zip"
+    supported_extensions = "zip"
 
     if "content_type" in asset:
         if asset["content_type"] in supported_content_types:
@@ -114,19 +109,28 @@ class GitHubPackageSource(BasePackageSource):
         release = api.get_release_by_tag_name(tag=version)
         return _to_version_info(release)
 
-    def fetch_version(self, version: str, option: str, operation: Operation, on_progress: ProgressCallback = progress_noop) -> None:
+    def fetch_version(
+        self,
+        version: str,
+        option: str,
+        operation: Operation,
+        on_progress: ProgressCallback = progress_noop,
+    ) -> None:
         on_step_progress = StepProgress.from_step_count(
-            step_count=1, on_progress=on_progress)
+            step_count=1, on_progress=on_progress
+        )
 
         api = self.get_api()
         release = api.get_release_by_tag_name(tag=version)
         release_id = release["id"]
-        assets = [asset for asset in api.list_release_assets(
-            release_id=release_id) if _is_usable_archive(asset)]
+        assets = [
+            asset
+            for asset in api.list_release_assets(release_id=release_id)
+            if _is_usable_archive(asset)
+        ]
 
         try:
-            asset = next(
-                (asset for asset in assets if asset["name"] == option))
+            asset = next((asset for asset in assets if asset["name"] == option))
         except StopIteration as exc:
             raise ValueError(f"unknown option: {option}") from exc
 

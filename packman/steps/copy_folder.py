dd66ddddc86c56848b9642e708448c2d7a9a3bd9
Ashleigh Udoh <mail@audoh.co.uk>
2021-01-20 00:23:15 +0000

    copy_folder now uses globbing for the src path

--- packman/steps/copy_folder.py
+++ packman/steps/copy_folder.py
@@ -1,16 +1,18 @@
 import os
+from glob import glob
 from pathlib import Path, PurePath
 from typing import Dict, List, Optional
 
 from loguru import logger
 from packman.models.install_step import BaseInstallStep, install_step
 from packman.utils.operation import Operation
-from packman.utils.progress import ProgressCallback, StepProgress, progress_noop
+from packman.utils.progress import (ProgressCallback, StepProgress,
+                                    progress_noop)
 
 
 @install_step("copy_folder")
 class CopyFolderInstallStep(BaseInstallStep):
-    name: str
+    glob: str
     to: str
     exclude: Optional[List[str]] = None
 
@@ -21,40 +23,35 @@ class CopyFolderInstallStep(BaseInstallStep):
         root_dir: str,
         on_progress: ProgressCallback = progress_noop,
     ) -> None:
-        src = ""
-        dest = ""
-        for root, subdirs, files in os.walk(package_path):
-            for subdir in subdirs:
-                if subdir == self.name:
-                    if src:
-                        raise FileExistsError(f"multiple folders found: {self.name}")
-                    src = os.path.join(root, subdir)
-                    dest = os.path.join(root_dir, self.to)
-                    break
+        src = glob(os.path.join(package_path, self.glob), recursive=True)
+        dest = os.path.join(root_dir, self.to)
         if not src:
-            logger.warning(f"folder not found: {self.name}")
+            logger.warning(f"folder not found: {self.glob}")
             on_progress(1.0)
             return
+        if len(src) > 1:
+            raise FileExistsError(f"multiple folders found: {self.glob}")
 
         files_to_copy: Dict[str, str] = {}
-        for root, subdirs, files in os.walk(src):
-            root_relpath = os.path.relpath(root, src)
-            dest_root = os.path.join(dest, root_relpath)
-            Path(dest_root).mkdir(parents=True, exist_ok=True)
-            for file in files:
-                file_src = os.path.join(root, file)
-                if self.exclude:
-                    file_relsrc = os.path.join(root_relpath, file)
-                    pure_path = PurePath(file_relsrc)
-                    if any(pure_path.match(pattern) for pattern in self.exclude):
-                        continue
-                file_dest = os.path.join(dest_root, file)
-                files_to_copy[file_src] = file_dest
+        for folder in src:
+            for root, _, files in os.walk(folder):
+                root_relpath = os.path.relpath(root, folder)
+                dest_root = os.path.join(dest, root_relpath)
+                Path(dest_root).mkdir(parents=True, exist_ok=True)
+                for file in files:
+                    file_src = os.path.join(root, file)
+                    if self.exclude:
+                        file_relsrc = os.path.join(root_relpath, file)
+                        pure_path = PurePath(file_relsrc)
+                        if any(pure_path.match(pattern) for pattern in self.exclude):
+                            continue
+                    file_dest = os.path.join(dest_root, file)
+                    files_to_copy[file_src] = file_dest
 
-        on_step_progress = StepProgress.from_step_count(
-            step_count=len(files_to_copy), on_progress=on_progress
-        )
+            on_step_progress = StepProgress.from_step_count(
+                step_count=len(files_to_copy), on_progress=on_progress
+            )
 
-        for file_src, file_dest in files_to_copy.items():
-            operation.copy_file(file_src, file_dest)
-            on_step_progress.advance()
+            for file_src, file_dest in files_to_copy.items():
+                operation.copy_file(file_src, file_dest)
+                on_step_progress.advance()

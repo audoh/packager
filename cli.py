ab923bce0ebf12a07ee6bedc47a8163fe355ab35
Ashleigh Udoh <mail@audoh.co.uk>
2021-02-14 16:27:11 +0000

    refactored cli.py to use a class PackmanCLI

--- cli.py
+++ cli.py
@@ -2,13 +2,14 @@ import os
 import shlex
 import sys
 from argparse import ArgumentError, ArgumentParser
-from typing import Optional
+from typing import Dict, List, Optional
 
 from loguru import logger
 
 from packman import InstallStep, PackageSource, Packman, sources, steps
 from packman.commands import (
     CleanCommand,
+    Command,
     ExportCommand,
     ImportCommand,
     InstallCommand,
@@ -21,7 +22,7 @@ from packman.commands import (
 )
 
 packman = Packman()
-commands = {
+DEFAULT_COMMANDS = {
     "install": InstallCommand(packman),
     "uninstall": UninstallCommand(packman),
     "list": InstalledPackageListCommand(packman),
@@ -35,8 +36,105 @@ commands = {
 }
 
 
+class PackmanCLI:
+    def __init__(
+        self,
+        commands: Dict[str, Command] = DEFAULT_COMMANDS,
+        no_interactive_mode: bool = False,
+    ) -> None:
+        desc = "Rudimentary file package management intended for modifications for games such as KSP and RimWorld"
+        parser = ArgumentParser(description=desc)
+        command_parsers = parser.add_subparsers(
+            metavar="<command>", help="Valid commands:", dest="command", required=False
+        )
+        for name, command in commands.items():
+            command_parser = command_parsers.add_parser(name, help=command.help)
+            command.configure_parser(command_parser)
+
+        self.commands = commands
+        self.parser = parser
+        self.command_parsers = command_parsers
+        self.interactive_mode_enabled = not no_interactive_mode
+        self.command_parsers.required = not self.interactive_mode_enabled
+        self.interactive_mode = False
+
+        self.update_usage()
+
+    def update_usage(self) -> None:
+        self.parser.usage = self.parser.format_help()[7:]
+
+    def print(self, value: str) -> None:
+        print(value)
+
+    def start_interactive_mode(self) -> None:
+        if self.interactive_mode:
+            return
+        self.interactive_mode = True
+
+        parser = self.parser
+        command_parsers = self.command_parsers
+
+        setattr(parser, "exit_on_error", False)
+        command_parsers.required = True
+        command_parsers.add_parser("exit", help="Quits this interactive session")
+        self.parser.format_help
+        self.update_usage()
+
+        self.print("Packman interactive session started.")
+        self.print(
+            "Type \u0022exit\u0022 to quit or \u0022--help\u0022 for more information."
+        )
+
+        while True:
+            raw = input("> ")
+            argv = shlex.split(raw)
+            if argv:
+                arg0 = argv[0].lower()
+                if arg0.startswith(
+                    ("exit", "quit", "stop", "abort", "goaway", "cancel")
+                ) or arg0 in (
+                    "q",
+                    "e",
+                ):
+                    self.stop_interactive_mode()
+                    break
+            try:
+                args = parser.parse_args(argv)
+                args_dict = vars(args)
+                command_name = args_dict.pop("command")
+                assert command_name is not None, "command_name cannot be None"
+                command = self.commands[command_name]
+                command.execute_safe(**args_dict)
+            except ArgumentError as exc:
+                self.print(f"error: {exc.message}")
+            except Exception as exc:
+                self.print(f"error: {exc}")
+            except SystemExit as exc:
+                if exc.code != 2:
+                    raise
+
+    def stop_interactive_mode(self) -> None:
+        if not self.interactive_mode:
+            return
+        self.command_parsers.required = False
+        self.interactive_mode = False
+
+    def parse(self, argv: List[str]) -> None:
+        parser = self.parser
+        args = parser.parse_args(argv)
+        args_dict = vars(args)
+        command_name: Optional[str] = args_dict.pop("command")
+        if command_name is None:
+            if self.interactive_mode_enabled:
+                self.start_interactive_mode()
+            else:
+                raise Exception("no command provided")
+        else:
+            command = DEFAULT_COMMANDS[command_name]
+            command.execute_safe(**args_dict)
+
+
 if __name__ == "__main__":
-    # TODO use cmd module
     # TODO set up docs generation, including the yaml schema
     # TODO add autocomplete
 
@@ -53,58 +151,5 @@ if __name__ == "__main__":
     logger.add(sys.stderr, level=os.environ.get("PACKMAN_LOGGING", "CRITICAL"))
 
     # Set up parser
-    parser = ArgumentParser(
-        description="Rudimentary file package management intended for modifications for games such as KSP and RimWorld"
-    )
-    command_parsers = parser.add_subparsers(
-        metavar="<command>", help="Valid commands:", dest="command", required=False
-    )
-    for name, command in commands.items():
-        command_parser = command_parsers.add_parser(name, help=command.help)
-        command.configure_parser(command_parser)
-    parser.usage = parser.format_help()[7:]
-
-    # Run
-    args = parser.parse_args(sys.argv[1:])
-    args_dict = vars(args)
-    command_name: Optional[str] = args_dict.pop("command")
-    if command_name is None:
-        parser.exit_on_error = False
-        command_parsers.required = True
-        command_parsers.add_parser("exit", help="Quits this interactive session")
-        parser.usage = parser.format_help()[7:]
-
-        print("Packman interactive session started.")
-        print(
-            "Type \u0022exit\u0022 to quit or \u0022--help\u0022 for more information."
-        )
-
-        while True:
-            raw = input("> ")
-            argv = shlex.split(raw)
-            if argv:
-                arg0 = argv[0].lower()
-                if arg0.startswith(
-                    ("exit", "quit", "stop", "abort", "goaway", "cancel")
-                ) or arg0 in (
-                    "q",
-                    "e",
-                ):
-                    sys.exit(0)
-            try:
-                args = parser.parse_args(argv)
-                args_dict = vars(args)
-                command_name = args_dict.pop("command")
-                command = commands[command_name]
-                command.execute_safe(**args_dict)
-            except ArgumentError as exc:
-                print(f"error: {exc.message}")
-            except Exception as exc:
-                print(f"error: {exc}")
-            except SystemExit as exc:
-                if exc.code != 2:
-                    raise
-
-    else:
-        command = commands[command_name]
-        command.execute_safe(**args_dict)
+    cli = PackmanCLI(commands=DEFAULT_COMMANDS)
+    cli.parse(argv=sys.argv[1:])
--- packman/commands/__init__.py
+++ packman/commands/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+from .command import Command
 from .exports import ExportCommand, ImportCommand
 from .installation import InstallCommand, UninstallCommand
 from .meta import (

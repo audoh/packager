21bb578df3ba2746a3bcb39b03e173fe4eef4833
Ashleigh Udoh <mail@audoh.co.uk>
2021-02-15 23:33:48 +0000

    patch temp dir in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from typing import Generator
+from unittest.mock import patch
 from uuid import uuid4
 
 import pytest
@@ -8,16 +9,16 @@ from packman import Packman
 
 
 @pytest.fixture(scope="function")
-def packman(mock_files: str) -> Generator[Packman, None, None]:
+def packman(mock_path: str) -> Generator[Packman, None, None]:
     yield Packman(
-        root_dir=os.path.join(mock_files, "mockgame"),
-        config_dir=os.path.join(mock_files, "mockconfigs"),
-        manifest_path=os.path.join(mock_files, "mockgame", "manifest.json"),
+        root_dir=os.path.join(mock_path, "mockgame"),
+        config_dir=os.path.join(mock_path, "mockconfigs"),
+        manifest_path=os.path.join(mock_path, "mockgame", "manifest.json"),
     )
 
 
 @pytest.fixture(scope="function", autouse=True)
-def mock_files() -> Generator[str, None, None]:
+def mock_path() -> Generator[str, None, None]:
     mock_path = os.path.join("tests_tmp", str(uuid4()))
     root_path = os.path.join(os.path.dirname(__file__), "fixtures", "files")
     shutil.copytree(root_path, mock_path)
@@ -26,19 +27,21 @@ def mock_files() -> Generator[str, None, None]:
 
 
 @pytest.fixture(scope="function", autouse=True)
-def reset_temp_files() -> Generator[None, None, None]:
-    yield
+def temp_path() -> Generator[str, None, None]:
+    mock_path = os.path.join("tmp", str(uuid4()))
+    with patch("tempfile.tempdir", mock_path):
+        yield mock_path
     try:
-        shutil.rmtree("/tmp")
+        shutil.rmtree(mock_path)
     except FileNotFoundError:
         pass
 
 
-def file_path_generator(mock_files: str) -> Generator[str, None, None]:
+def file_path_generator(mock_path: str) -> Generator[str, None, None]:
     while True:
-        yield os.path.join(mock_files, str(uuid4()))
+        yield os.path.join(mock_path, str(uuid4()))
 
 
 @pytest.fixture(scope="function")
-def file_paths(mock_files: str) -> Generator[Generator[str, None, None], None, None]:
-    yield file_path_generator(mock_files)
+def file_paths(mock_path: str) -> Generator[Generator[str, None, None], None, None]:
+    yield file_path_generator(mock_path)

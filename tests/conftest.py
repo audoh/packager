0d9f4a0475217cde708e75aee50db5311fdd26ce
Ashleigh Udoh <mail@audoh.co.uk>
2021-02-15 22:48:55 +0000

    fixed tmp files persisting across tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,10 +17,20 @@ def packman(mock_files: str) -> Generator[Packman, None, None]:
 
 @pytest.fixture(scope="function", autouse=True)
 def mock_files() -> Generator[str, None, None]:
+    dirname = "/test"
     root_path = os.path.join(os.path.dirname(__file__), "fixtures", "files")
     try:
-        shutil.rmtree("/test")
+        shutil.rmtree(dirname)
     except FileNotFoundError:
         pass
-    shutil.copytree(root_path, "/test")
-    yield "/test"
+    shutil.copytree(root_path, dirname)
+    yield dirname
+
+
+@pytest.fixture(scope="function", autouse=True)
+def reset_temp_files() -> Generator[None, None, None]:
+    try:
+        shutil.rmtree("/tmp")
+    except FileNotFoundError:
+        pass
+    yield

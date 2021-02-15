54d1fdbb6b578e795b44b7cd04b792c6e8e4b6eb
Ashleigh Udoh <mail@audoh.co.uk>
2021-02-15 21:51:58 +0000

    mock_files yields str used by packman

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,20 @@ from packman import Packman
 
 
 @pytest.fixture(scope="function")
-def packman() -> Generator[Packman, None, None]:
+def packman(mock_files: str) -> Generator[Packman, None, None]:
     yield Packman(
-        root_dir="/test/mockgame",
-        config_dir="/test/mockconfigs",
-        manifest_path="/test/mockgame/manifest.json",
+        root_dir=os.path.join(mock_files, "mockgame"),
+        config_dir=os.path.join(mock_files, "mockconfigs"),
+        manifest_path=os.path.join(mock_files, "mockgame", "manifest.json"),
     )
 
 
 @pytest.fixture(scope="function", autouse=True)
-def mock_files() -> Generator[None, None, None]:
+def mock_files() -> Generator[str, None, None]:
     root_path = os.path.join(os.path.dirname(__file__), "fixtures", "files")
     try:
         shutil.rmtree("/test")
     except FileNotFoundError:
         pass
     shutil.copytree(root_path, "/test")
-    yield
+    yield "/test"

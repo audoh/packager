81811a444d1475011ab160cd6179aa757fefe830
Ashleigh Udoh <ash@audoh.co.uk>
2021-02-20 22:52:56 +0000

    refactored operation tests to be better

--- tests/operation/test_copy_file.py
+++ tests/operation/test_copy_file.py
@@ -1,65 +1,51 @@
 import os
 from typing import Iterator
 
+import pytest
 from packman.utils.operation import Operation
 
 
-def test_can_copy_file(file_paths: Iterator[str]) -> None:
+@pytest.mark.parametrize("data", ["the data"])
+def test_copy_file(file_paths: Iterator[str], data: str) -> None:
     src_path = next(file_paths)
     dest_path = next(file_paths)
     with open(src_path, "w") as fp:
-        fp.write("mocktext")
+        fp.write(data)
 
     op = Operation()
     op.copy_file(src_path, dest_path)
 
+    # Test copy
     assert os.path.exists(dest_path), "dest file should be created"
     with open(dest_path, "r") as fp:
-        assert fp.read() == "mocktext", "dest file should have src contents"
+        assert fp.read() == data, "dest file should have src contents"
+
+    # Test rollback
+    op.restore()
+    assert not os.path.exists(dest_path), "dest file should be deleted after restore"
 
 
-def test_can_overwrite_file(file_paths: Iterator[str]) -> None:
+@pytest.mark.parametrize("new_data,old_data", [("new data", "old data")])
+def test_copy_and_overwrite_file(
+    file_paths: Iterator[str], new_data: str, old_data: str
+) -> None:
     src_path = next(file_paths)
     dest_path = next(file_paths)
     with open(src_path, "w") as fp:
-        fp.write("mocktext")
+        fp.write(new_data)
     with open(dest_path, "w") as fp:
-        fp.write("otherfile")
+        fp.write(old_data)
 
     op = Operation()
     op.copy_file(src_path, dest_path)
 
+    # Test copy
     assert os.path.exists(dest_path), "dest file should exist"
     with open(dest_path, "r") as fp:
-        assert fp.read() == "mocktext", "dest file should have src contents"
+        assert fp.read() == new_data, "dest file should have src contents"
 
-
-def test_backs_up_overwritten_file(file_paths: Iterator[str]) -> None:
-    src_path = next(file_paths)
-    dest_path = next(file_paths)
-    with open(src_path, "w") as fp:
-        fp.write("mocktext")
-    with open(dest_path, "w") as fp:
-        fp.write("otherfile")
-
-    op = Operation()
-    op.copy_file(src_path, dest_path)
-
-    assert dest_path in op.backups, "operation should store backup path"
-    assert os.path.exists(
-        op.backups[dest_path]
-    ), "operation should copy overwitten file to the backup path"
-
-
-def test_overwritten_file_restored(file_paths: Iterator[str]) -> None:
-    src_path = next(file_paths)
-    dest_path = next(file_paths)
-    with open(src_path, "w") as fp:
-        fp.write("mocktext")
-    with open(dest_path, "w") as fp:
-        fp.write("otherfile")
-
-    op = Operation()
-    op.copy_file(src_path, dest_path)
-
+    # Test rollback
     op.restore()
+    assert os.path.exists(dest_path), "dest file should still exist"
+    with open(dest_path, "r") as fp:
+        assert fp.read() == old_data

6d7aaed8dfdbbdd0c33ea5f457cf7a4b8c288c81
Ashleigh Udoh <ash@audoh.co.uk>
2021-02-21 17:20:49 +0000

    test output format supports parameterised fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 from itertools import count
 from sys import stderr
@@ -7,6 +8,7 @@ from unittest.mock import patch
 from uuid import uuid4
 
 import pytest
+from _pytest.fixtures import SubRequest
 from loguru import logger
 from packman import Packman
 from pytest import Item
@@ -42,17 +44,22 @@ def temp_path() -> Generator[str, None, None]:
         pass
 
 
-def _file_path_generator(mock_path: str) -> Generator[str, None, None]:
+def _file_path_generator(root_path: str) -> Generator[str, None, None]:
     while True:
-        path = os.path.join(mock_path, str(uuid4()))
+        path = os.path.join(root_path, str(uuid4()))
         logger.debug(f"generated {path=}")
         yield path
 
 
-@pytest.fixture(scope="function")
-def file_paths(mock_path: str) -> Generator[Generator[str, None, None], None, None]:
+@pytest.fixture(scope="function", params=[{"nested": False}])
+def file_paths(
+    request: SubRequest, mock_path: str
+) -> Generator[Generator[str, None, None], None, None]:
     """ Returns a generator which can be used to get file paths for creating test files. """
-    yield _file_path_generator(mock_path)
+    root_path = (
+        os.path.join(mock_path, str(uuid4())) if request.param["nested"] else mock_path
+    )
+    yield _file_path_generator(root_path)
 
 
 @pytest.fixture(scope="function")
@@ -85,19 +92,23 @@ def pytest_itemcollected(item: Item) -> None:
     name = nodeid[:args_idx] + nodeid[args_idx_end + 1 :]
     args_str = nodeid[args_idx + 1 : args_idx_end]
 
+    logger.debug(f"processing {item._nodeid=}")
+
     args_dict: Dict[str, str] = {}
-    for mark in item.iter_markers("parametrize"):
+    markers = list(item.iter_markers("parametrize"))
+    for mark in reversed(markers):
         names_raw = mark.args[0]
-        names: List[str] = (
-            [name.strip() for name in names_raw.split(",")]
+        names = (
+            [name.strip() for name in reversed(names_raw.split(","))]
             if isinstance(names_raw, str)
-            else list(names_raw)
+            else list(reversed(names_raw))
         )
 
         param_sets = list(mark.args[1])
         for param_name, param_index in zip(names, count(start=0)):
             # Find node's value for this param
             found_any = False
+            match_len: int = 0
             longest_match: str = ""
             value: str = ""
             for param_set in param_sets:
@@ -109,21 +120,27 @@ def pytest_itemcollected(item: Item) -> None:
                 )
                 param_str = _str(param)
 
-                # Find longest value that args_str starts with
-                if len(longest_match) < len(param_str) and args_str.startswith(
-                    param_str
-                ):
-                    found_any = True
-                    longest_match = param_str
-                    value = repr(param)
+                # Find longest value that args_str ends with (possibly with an integer suffix)
+                escaped = re.escape(param_str)
+                regex = re.compile(escaped + r"[0-9]*$")
+                if len(longest_match) < len(param_str):
+                    match = regex.search(args_str)
+                    if match:
+                        found_any = True
+                        longest_match = param_str
+                        match_len = len(match.group(0))
+                        value = repr(param)
 
             if not found_any:
                 raise ValueError(
-                    f"failed to resolve {param_name=} to param value from {param_sets=}"
+                    f"failed to resolve {param_name=} to param value from {param_sets=}, {args_str=}"
                 )
 
-            args_str = args_str[len(longest_match) + 1 :]
+            args_str = args_str[: -(match_len + 1)]
             args_dict[param_name] = value
 
-    args_str = ", ".join((f"{key}: {value}" for key, value in args_dict.items()))
+    if args_str:
+        args_str = ", ".join(args_str.split("-")) + ", "
+
+    args_str += ", ".join((f"{key}: {value}" for key, value in args_dict.items()))
     item._nodeid = f"{name} ({args_str})"

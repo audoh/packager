eae396a96b31b9d923e0a0ef1bc25d532d2e4347
Ashleigh Udoh <ash@audoh.co.uk>
2021-04-25 18:37:10 +0100

    updated packman.sh to the improved runner from request-file

--- bin/packman.sh
+++ bin/packman.sh
@@ -1,1 +1,23 @@
-poetry run python -m packman_cli.cli $*
+#!/bin/bash -e
+
+log() {
+  if [ -z "$PACKMANDEBUG" ]; then
+    return 0
+  fi
+  echo $@ >&2
+}
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
+ROOT_DIR="$SCRIPT_DIR/.."
+
+# Activate venv
+log Activating venv
+pushd "$ROOT_DIR" > /dev/null
+VENV_DIR=`poetry env info -p`
+source "$VENV_DIR/bin/activate"
+popd > /dev/null
+
+# Run script
+log Running script
+export PYTHONPATH="$ROOT_DIR/packman_cli"
+python -m packman_cli.cli "$@"
